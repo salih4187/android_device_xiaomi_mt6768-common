8c36f9cadebc1504c1fb6542dcdcb962d3ef7d79
Salih Çağlar <salihcaglar4187@gmail.com>
2026-05-25 21:05:37 +0300

    Revert "mt6768-common: prebuilts: add GCamGo"
    This reverts commit 639d3d1c3a8d1b36191e548da0a988b8be3ec40c.

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -225,10 +225,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
                                 
-# Camera
-PRODUCT_PACKAGES += \
-    GoogleCameraGo
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(COMMON_PATH)
 
